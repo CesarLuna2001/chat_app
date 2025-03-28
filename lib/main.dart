ad383dccd3b1a0267397540b48b10510f0129145
Cesarluna200115 <cesarluna200115@outlook.com>
2025-03-27 21:08:53 -0600

    MyMessageBubble

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:chat_app/config/theme/app_theme.dart';
-import 'package:chat_app/presentation/chat/chat_screen.dart';
+import 'package:chat_app/presentation/screens/chat/chat_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Yes No App',
       debugShowCheckedModeBanner: false,
-      theme: AppTheme(selectedColor: 6).theme(),
+      theme: AppTheme(selectedColor: 2).theme(),
       home: const ChatScreen(),
     );
   }
