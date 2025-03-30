e10f53ea20e76174a9dc1ca776ccdd621934895a
Cesarluna200115 <cesarluna200115@outlook.com>
2025-03-29 22:36:32 -0600

    LoadingBuilder

--- lib/presentation/widgets/chat/her_message_bubble.dart
+++ lib/presentation/widgets/chat/her_message_bubble.dart
@@ -43,6 +43,16 @@ class _ImageBubble extends StatelessWidget {
         width: size.width * 0.7,
         height: 150,
         fit: BoxFit.cover,
+        loadingBuilder: (context, child, loadingProgress) {
+          if (loadingProgress == null) return child;
+
+          return Container(
+            width: size.width * 0.7,
+            height: 150,
+            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+            child: const Text("Chat Screen está enviando un mensaje") ,
+          );
+        },
       ));
   }
 }
